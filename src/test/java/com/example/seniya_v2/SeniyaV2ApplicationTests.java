c075d5fe88acae99de9f8746efcb4543e63f50e5
chang0506 <j8970@naver.com>
2025-07-03 12:05:57 +0900

    project

---
+++ src/test/java/com/example/seniya_v2/SeniyaV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seniya_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniyaV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
