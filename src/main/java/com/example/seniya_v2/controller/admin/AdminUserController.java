ee9b69d772ab74a014781e3dad7fe6144c0fa9f8
buzz9248 <buzz9248@naver.com>
2025-07-08 14:21:12 +0900

    feat: api-mapping-pattern created

--- src/main/java/com/example/seniya_v2/controller/admin/AdminUserController.java
+++ src/main/java/com/example/seniya_v2/controller/admin/AdminUserController.java
@@ -1,4 +1,15 @@
 package com.example.seniya_v2.controller.admin;
 
+import com.example.seniya_v2.common.constants.ApiMappingPattern;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(ApiMappingPattern.ADMIN_USER_API)
 public class AdminUserController {
+
+
+
 }
