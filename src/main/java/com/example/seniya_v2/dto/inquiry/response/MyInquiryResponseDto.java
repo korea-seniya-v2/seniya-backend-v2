db0ef298d18189271416df5b96eb9c7210c65037
chang0506 <j8970@naver.com>
2025-07-23 12:27:33 +0900

    refactor:inquiry

--- src/main/java/com/example/seniya_v2/dto/inquiry/response/MyInquiryResponseDto.java
+++ src/main/java/com/example/seniya_v2/dto/inquiry/response/MyInquiryResponseDto.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MyInquiryResponseDto {
+    private Long inquiryId;
     private String title;
     private InquiryStatus status;
     private LocalDateTime createdAt;
